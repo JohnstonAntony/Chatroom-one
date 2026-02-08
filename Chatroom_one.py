c88a62d36635804a567816e867c996136d114750
Johnston Antony <Johnston4u19@gmail.com>
2026-02-08 20:29:37 +0000

    Added folders and tidied up a bit so it's easier to proceed.

--- Chatroom_one.py
+++ Chatroom_one.py
@@ -1,5 +1,5 @@
 # pip install Flask
 # pip install flask-socketio
 
-# make sure to install both of these bro 
+# make sure to install both of these bro if pip isn't found in command try using pip3 install ... 
 
