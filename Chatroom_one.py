86693ad5ce45ab0caca2e82270749003a7e5dcce
Johnston Antony <Johnston4u19@gmail.com>
2026-02-09 21:25:13 +0000

    basic messaging flask code implemented.

--- Chatroom_one.py
+++ Chatroom_one.py
@@ -4,7 +4,7 @@
 # make sure to install both of these bro if pip isn't found in command try using pip3 install ... 
 
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -12,6 +12,13 @@ socketio = SocketIO(app)
 @app.route('/')
 def index():
     return render_template("home.html")
+
+@socketio.on('message')
+def handle_message(msg):
+    emit('message', msg, broadcast=True) 
+    # this bit emits the message to everyone, read the pusedo code in the word doc to see what we should try. 
+
+
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
