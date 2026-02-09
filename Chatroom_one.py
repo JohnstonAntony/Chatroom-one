bdd02a5d60b3a1799c9506d6c410aa60d9e2bb45
CarlosGabrielBristol <carlosbristol29@gmail.com>
2026-02-09 14:31:41 +0000

    Installing Flask and flask-socketio using pip

--- Chatroom_one.py
+++ Chatroom_one.py
@@ -3,3 +3,16 @@
 
 # make sure to install both of these bro if pip isn't found in command try using pip3 install ... 
 
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return "Chatroom is online!"
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
+
